b98c6c2d63bcd5e531c58cb74a56746bfcefdda6
Yen Colon <yendejesusc41@gmail.com>
2020-08-09 02:04:00 -0400

    adding font support

--- src/components/Menu/MenuBottomBrandComponent.js
+++ src/components/Menu/MenuBottomBrandComponent.js
@@ -5,7 +5,7 @@ function MenuBottomBrand({ title, image, style }) {
     return (
         <div className='flex flex-col items-center justify-center full-w mb-20' style={style}>
             <img className='rounded-full h-20 w-20 flex items-center justify-center bg-black' src={image} alt='logo' height={80} width={80} />
-            <h1 className='text-xl'>{title}</h1>
+            {/* <h1 className='text-xl'>{title}</h1> */}
         </div>
     );
 }
