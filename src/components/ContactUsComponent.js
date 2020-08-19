23437ff15023216055d4af7cdd069fc917d7d1fa
Yen Colon <yendejesusc41@gmail.com>
2020-08-18 22:01:38 -0400

    testing contacts

--- src/components/ContactUsComponent.js
+++ src/components/ContactUsComponent.js
@@ -8,32 +8,32 @@ import twitter from 'assets/images/twitter.svg';
 import whatsapp from 'assets/images/whatsapp.svg';
 import facebook from 'assets/images/facebook.svg';
 
-// function SocialNetwork({ network }) {
-//     switch (network) {
-//         case 'instagram':
-//             return (
-//                 // <InstagramOutlined className='text-4xl mr-5' />
-//                 <img src={instagram} className="w-10 m-2" alt='instagram' />// <div>Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
-//             )
-//         case 'twitter':
-//             return (
-//                 <img src={twitter} className="w-10 m-2" alt='twitter' />//<TwitterOutlined className='text-4xl mr-5' />
-//             )
-//         case 'facebook':
-//             return (
-//                 <img src={facebook} className="w-10 m-2" alt='facebook' />//<FacebookOutlined className='text-4xl mr-5' />
-//             )
-//         case 'whatsapp':
-//             return (
-//                 <img src={whatsapp} className="w-10 m-2" alt='whatsapp' />//<WhatsAppOutlined className='text-4xl mr-5' />
-//             )
-//         case 'telegram':
-//             return (
-//                 <img src={telegram} className="w-10 m-2" alt='telegram' />
-//             )
-//         default: return <div></div>
-//     }
-// }
+function SocialNetwork({ network }) {
+    switch (network) {
+        case 'instagram':
+            return (
+                // <InstagramOutlined className='text-4xl mr-5' />
+                <img src={instagram} className="w-10 m-2" alt='instagram' />// <div>Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
+            )
+        case 'twitter':
+            return (
+                <img src={twitter} className="w-10 m-2" alt='twitter' />//<TwitterOutlined className='text-4xl mr-5' />
+            )
+        case 'facebook':
+            return (
+                <img src={facebook} className="w-10 m-2" alt='facebook' />//<FacebookOutlined className='text-4xl mr-5' />
+            )
+        case 'whatsapp':
+            return (
+                <img src={whatsapp} className="w-10 m-2" alt='whatsapp' />//<WhatsAppOutlined className='text-4xl mr-5' />
+            )
+        case 'telegram':
+            return (
+                <img src={telegram} className="w-10 m-2" alt='telegram' />
+            )
+        default: return <div></div>
+    }
+}
 
 function Section({ title, childComponent, sectionStyle }) {
     return (
@@ -75,7 +75,7 @@ function ContactUs({ title, image, socialLinks, workingDays, containerStyle }) {
                             socialLinks.map(social => {
                                 return (
                                     <div onClick={() => { window.open(social.url, "_blank") }}>
-                                        {/* <SocialNetwork network={social.network} /> */}
+                                        <SocialNetwork network={social.network} />
                                     </div>
                                 )
                             })
