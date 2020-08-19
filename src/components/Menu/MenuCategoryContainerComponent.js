39902c8edd2e8cf17b31ce4312f71770774e1e60
Yen Colon <yendejesusc41@gmail.com>
2020-08-19 01:29:39 -0400

    testing category component

--- src/components/Menu/MenuCategoryContainerComponent.js
+++ src/components/Menu/MenuCategoryContainerComponent.js
@@ -21,13 +21,12 @@ function RenderAs({ items, renderAs = 'text' , style}) {
 
 function MenuCategoryContainer({ title, description, items, renderAs = 'text', innerContainerStyle = {}, containerStyle = {}, cardStyle ={}, headingStyle = {} }) {
     return (
-        <div>Hol</div>
-        // <div className='mt-5 mb-20 mx-5 md:mx-5 md:mt-5 md:mb-20 flex flex-col content-center justify-center' style={containerStyle}>
-        //     <h1 className='font-bold subpixel-antialiased text-2xl text-center mb-3' style={headingStyle}>{title}</h1>
-        //     <div style={innerContainerStyle}>
-        //         <RenderAs items={items} renderAs={renderAs} style={cardStyle}/>
-        //     </div>
-        // </div>
+        <div className='mt-5 mb-20 mx-5 md:mx-5 md:mt-5 md:mb-20 flex flex-col content-center justify-center' style={containerStyle}>
+            <h1 className='font-bold subpixel-antialiased text-2xl text-center mb-3' style={headingStyle}>{title}</h1>
+            <div style={innerContainerStyle}>
+                <RenderAs items={items} renderAs={renderAs} style={cardStyle}/>
+            </div>
+        </div>
     );
 }
 
