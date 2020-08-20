f2a1dc959762ad84f99949c3004b360bf751feba
Yen Colon <yendejesusc41@gmail.com>
2020-08-19 20:46:48 -0400

    fixing social links

--- src/components/ContactUsComponent.js
+++ src/components/ContactUsComponent.js
@@ -6,43 +6,6 @@ import twitter from 'assets/images/twitter.svg';
 import whatsapp from 'assets/images/whatsapp.svg';
 import facebook from 'assets/images/facebook.svg';
 
-function SocialNetwork({ network = 'instagram' }) {
-
-    switch (network) {
-        case 'instagram':
-            return (
-                // <InstagramOutlined className='text-4xl mr-5' />
-                <div>
-                    {/* <img src={instagram} className="w-10 m-2" alt='instagram' height={20} width={20} /> */}
-                </div>// <div>Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
-            )
-        case 'twitter':
-            return (
-                <div>
-                    {/* <img src={twitter} className="w-10 m-2" alt='twitter' height={20} width={20} /> */}
-                </div>//<TwitterOutlined className='text-4xl mr-5' />
-            )
-        case 'facebook':
-            return (
-                <div>
-                    {/* <img src={facebook} className="w-10 m-2" alt='facebook' height={20} width={20} /> */}
-                </div>//<FacebookOutlined className='text-4xl mr-5' />
-            )
-        case 'whatsapp':
-            return (
-                <div>
-                    {/* <img src={whatsapp} className="w-10 m-2" alt='whatsapp' height={20} width={20} /> */}
-                </div>//<WhatsAppOutlined className='text-4xl mr-5' />
-            )
-        case 'telegram':
-            return (
-                <div>
-                    {/* <img src={telegram} className="w-10 m-2" alt='telegram' height={20} width={20} /> */}
-                </div>
-            )
-        default: return <div></div>
-    }
-}
 
 function Section({ title, childComponent, sectionStyle }) {
     return (
@@ -54,6 +17,43 @@ function Section({ title, childComponent, sectionStyle }) {
 }
 
 function ContactUs({ title, image, socialLinks, workingDays, containerStyle }) {
+
+    const socialNetwork = (network) => {
+        switch (network) {
+            case 'instagram':
+                return (
+                    <div>
+                        <img src={instagram} className="w-10 m-2" alt='instagram' height={20} width={20} />
+                    </div>// <div>Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
+                )
+            case 'twitter':
+                return (
+                    <div>
+                        <img src={twitter} className="w-10 m-2" alt='twitter' height={20} width={20} />
+                    </div>//<TwitterOutlined className='text-4xl mr-5' />
+                )
+            case 'facebook':
+                return (
+                    <div>
+                        <img src={facebook} className="w-10 m-2" alt='facebook' height={20} width={20} />
+                    </div>//<FacebookOutlined className='text-4xl mr-5' />
+                )
+            case 'whatsapp':
+                return (
+                    <div>
+                        <img src={whatsapp} className="w-10 m-2" alt='whatsapp' height={20} width={20} />
+                    </div>//<WhatsAppOutlined className='text-4xl mr-5' />
+                )
+            case 'telegram':
+                return (
+                    <div>
+                        <img src={telegram} className="w-10 m-2" alt='telegram' height={20} width={20} />
+                    </div>
+                )
+            default: return <div></div>
+        }
+    }
+
     return (
         <div className='mb-20'>
             <h1 className='text-xl text-center font-bold'>Contactos</h1>
@@ -82,7 +82,7 @@ function ContactUs({ title, image, socialLinks, workingDays, containerStyle }) {
                             socialLinks.map(social => {
                                 return (
                                     <div key={social.network} onClick={() => { window.open(social.url, "_blank") }} >
-                                        <img src={telegram} className="w-10 m-2" alt='telegram' height={20} width={20} />
+                                       {socialNetwork(social.network)}
                                     </div>
                                 )
                             })
