48424267ff3b118809f6ad3408b76a7b05d6ed13
Yen Colon <yendejesusc41@gmail.com>
2020-08-28 04:02:05 -0400

    remocing unused import

--- src/shared/MenuStyle.js
+++ src/shared/MenuStyle.js
@@ -1,5 +1,3 @@
-
-import logo from '../assets/images/logo.png'
 //color
 export const primaryColor = '#F2AF29';
 export const secondaryColor = '#DB3A34';
@@ -10,38 +8,41 @@ export const showCategoriesMenuAs = 'buttons';
 export const showDishAs = 'text';
 export const stickButtonsMenu = false;
 export const name = 'el taco.';
-export const imageLogo = logo;
+export const imageLogo = 'logo.png';
 export const hasDelivery = true;
+
 //fonts
 export const titleFontFamily = 'Oleo Script';
 export const mainFontFamily = 'Anaheim';
-
-
 export const menuCategoryFont = 'Artesania display'
 export const menuCardTitleFontFamily = 'Oleo Script'
 export const menuFontFamily = 'Arvo';
 //styles
+
 //Navigation bar 
 export const navBarContainerStyle = { backgroundColor: primaryColor, fontFamily: mainFontFamily, fontSize: '1.4em' }
 export const navTitleStyle = { color: 'white', 'font-weight': '700', fontFamily: titleFontFamily }
 //Navigation bar bottom
 export const navItemStyle = { color: 'white' }
 //Menu
+
 //Menu -> buttons top
 export const categoryButtonBackground = 'transparent';
 export const categoryButtonSelectedStyle = { borderRadius: '4px', backgroundColor: 'transparent', color: accentColor, border: '2px solid ' + accentColor,  fontFamily: menuFontFamily, fontSize: '15px' }
 export const categoryButtonStyle = {  borderRadius: '4px', backgroundColor: 'transparent', color: primaryColor, border: '2px solid ' + primaryColor, fontFamily: mainFontFamily, fontSize: '15px'}
+
 //Menu category container 
 export const menuStyle = {} //1
 export const menuContainerStyle = { fontFamily: menuCategoryFont,  color: accentColor, textAlign: 'center' } // 2 FONT
-
 export const categoryContainerStyle = {  } //3
 export const categoryHeadingStyle = {}
 export const categoryInnerContainer = { borderTop: '1.5px solid ' + accentColor, boxShadow: '0px 0px 0px 0px' }
-
 export const categoryCardStyle = { fontFamily: menuFontFamily, lineHeight: 1.3, color: 'rgba(0,0,0,1)' } //4  FONT
 export const categoryCardProdutcTitleStyle = { fontFamily: menuFontFamily}  // FONT
 export const categoryCardProdutcDescriptionStyle = { fontFamily: menuFontFamily }
+
+
+
 
 export const paymentCardStyle = { fontFamily: menuFontFamily }
 export const contactStyle = { fontFamily: menuFontFamily }
