16298b3e80d1370de4bb940b668bc1db38274e17
Yen Colon <yendejesusc41@gmail.com>
2020-08-29 01:31:15 -0400

    adding prod api url

--- src/shared/apiUrl.js
+++ src/shared/apiUrl.js
@@ -1,2 +1,2 @@
-export const API_URL = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'? 'http://localhost:3001/' :'https://hapticon-bdd-mock.herokuapp.com/'; ///'';
+export const API_URL = 'https://hapticon-bdd-mock.herokuapp.com/' //!process.env.NODE_ENV || process.env.NODE_ENV === 'development'? 'http://localhost:3001/' :'https://hapticon-bdd-mock.herokuapp.com/'; ///'';
 export const API_YADIO = 'https://api.yadio.io/json';
