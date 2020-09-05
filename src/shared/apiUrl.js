e1be70aeb7f49d23fe9ac47a3604643a9a13179d
Yen Colon <yendejesusc41@gmail.com>
2020-09-04 23:54:01 -0400

    fixing prod api_url

--- src/shared/apiUrl.js
+++ src/shared/apiUrl.js
@@ -1,4 +1,4 @@
-export const API_URL = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'? 'http://127.0.0.1:8000/api/' :'https://haptico-back.herokuapp.com/'; ///'';
+export const API_URL = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'? 'http://127.0.0.1:8000/api/' :'https://haptico-back.herokuapp.com/api/'; ///'';
 export const API_YADIO = 'https://api.yadio.io/json';
 
 //'https://hapticon-bdd-mock.herokuapp.com/' //
