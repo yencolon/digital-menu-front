68fb60f3779560f428862410bba26f8c97c324b7
Yen Colon <yendejesusc41@gmail.com>
2020-09-10 15:14:56 -0400

    fixing url api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Main from 'components/MainComponent';
 import './App.css'
 import MenuListComponent from 'components/MenuListComponent';
+import { API_URL } from 'shared/apiUrl';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/menu/:id" >
-            <Main colorSpin='#F2AF29' />
+            <Main colorSpin='#F2AF29' logo={API_URL + 'images/logo.png'} />
           </Route>
           <Route path="/customers">
             <MenuListComponent />
--- src/api/MenuApi.js
+++ src/api/MenuApi.js
@@ -1,6 +1,6 @@
 import { API_URL } from "shared/apiUrl"
 
-const prefix = 'menus/';
+const prefix = 'api/menus/';
 
 export const getMenu = async () => {
     await fetch(API_URL + prefix + '?_embed=categories')
