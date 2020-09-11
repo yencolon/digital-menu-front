beb2c4bf741848c8575abb4463e6f5fe840b3a2e
Yen Colon <yendejesusc41@gmail.com>
2020-09-10 21:03:23 -0400

    updating assets and logo style

--- src/components/Menu/MenuBannerComponent.js
+++ src/components/Menu/MenuBannerComponent.js
@@ -4,7 +4,7 @@ import { API_URL } from 'shared/apiUrl'
 export default function MenuBanner() {
     return (
         <div className='flex items-center justify-center mb-5'>
-            <img src={API_URL + '/images/Banner Ad Final.png'} alt='promo' ></img>
+            <img src={API_URL + 'images/banner.png'} alt='promo' ></img>
         </div>
     )
 }
