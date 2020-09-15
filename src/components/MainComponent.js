16e45ceae69386961585dcf9d0ca56bc957f56de
Yen Colon <yendejesusc41@gmail.com>
2020-09-15 02:32:30 -0400

    removing delivery view

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,7 +6,6 @@ import MenuHeader from 'components/common/MenuHeaderComponent';
 import MenuNavigation from 'components/common/MenuNavigationComponent';
 import ContactUs from 'components/ContactUsComponent';
 import PaymentMethods from 'components/PaymentMethods';
-import Delivery from 'components/DeliveryComponent';
 import GoogleFontLoader from 'react-google-font-loader';
 import { API_URL } from 'shared/apiUrl';
 import MenuBottomBrand from './common/MenuBottomBrandComponent';
@@ -48,21 +47,6 @@ const paymentMethods = [
     { name: 'Paypal', identification: 'pedroperez@gmail.com', owner: 'Pedro Perez', extrainfo: '', image: 'https://haptico-back.herokuapp.com/images/paypal.png' },
     { name: 'Airtm', identification: 'pedroperez@gmail.com', owner: 'Pedro Perez', extrainfo: '', image: 'https://haptico-back.herokuapp.com/images/airtm.png' },
     { name: 'Pago Móvil', identification: '041245454585', owner: 'Pedro Perez', extrainfo: '22111000', image: 'https://haptico-back.herokuapp.com/images/Pago Movil.jpg' }
-];
-
-const deliveries = [
-    { name: 'Alo', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/alo.jpg', url: 'https://wa.me/message/RAM5DY4A4KTYJ1' },
-    { name: 'Beefly', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/beefly.jpg', url: 'https://beefly.app/' },
-    { name: 'Detodito', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/detodito.jpg', url: 'https://uqr.to/gdsm' },
-    { name: 'Ecobike', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/ecobike.jpg', url: 'https://wa.me/584248399970' },
-    { name: 'Guick', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/guick.jpg', url: 'https://www.guick.com/' },
-    { name: 'Kepido', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/kepido.jpg', url: 'https://onelink.to/kp9bn7' },
-    { name: "Pa'llevar", description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/pallevar.jpg', url: 'https://comida.pidepallevar.com/es' },
-    { name: 'Rela', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/rela.jpg', url: 'https://wa.me/584147782469' },
-    { name: 'Ubiigo', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/ubiigo.jpg', url: 'https://ubiigo.co/app/link' },
-    { name: 'Valencia Delivery', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/valencia.jpg', url: 'https://www.deliveryvalencia.com' },
-    { name: 'Yolopio', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/yolopido.jpg', url: 'https://yolopido.com' },
-    { name: 'Yummy', description: 'Una descripcion pava del delivey jeje', image: 'https://haptico-back.herokuapp.com/images/yummy.jpg', url: 'https://pideyummy.com' }
 ];
 
 function LoadFonts ({ fonts }) {
@@ -117,7 +101,6 @@ function Main ({ colorSpin = '#097392', logo }) {
         categoryCardHighLightStyle,
         categoryContainerStyle,
         contactStyle,
-        deliveryCardStyle,
         imageLogo,
         mainFontFamily,
         menuFontFamily,
@@ -136,7 +119,6 @@ function Main ({ colorSpin = '#097392', logo }) {
         navItemStyle,
         stickButtonsMenu,
         categoryButtonSelectedStyle,
-        hasDelivery,
         categoryHeadingStyle,
         categoryInnerContainer,
         categoryCardProdutcDescriptionStyle,
@@ -196,18 +178,18 @@ function Main ({ colorSpin = '#097392', logo }) {
                             loadingComponent={TabsLoadingComponent}
                         />
                     </Route>
-                    <Route path={`${path}/delivery`}>
+                    {/* <Route path={`${path}/delivery`}>
                         <Delivery
                             titleStyle={sectionHeadingStyle}
                             deliveryCardStyle={deliveryCardStyle}
                             deliveries={deliveries}
                             loadingComponent={TabsLoadingComponent}
                         />
-                    </Route>
+                    </Route> */}
                     <Redirect from='*' to={url}></Redirect>
                 </Switch>
                 <MenuBottomBrand image={imageLogo} title={name} style={{}} />
-                <MenuNavigation containerStyle={navBarContainerStyle} itemStyle={navItemStyle} hasDelivery={hasDelivery} />
+                <MenuNavigation containerStyle={navBarContainerStyle} itemStyle={navItemStyle} hasDelivery={false} />
             </Fragment>
     );
 }
